5493ae9e60882ff68a03b73bc7281615be2db0dd
Tyra Cudjoe <tyra20015@gmail.com>
2022-08-31 14:33:36 +0200

    Styling: Added navbar w/ dropdown and hover

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -3,38 +3,47 @@ import { Hero } from './hero';
 export const HEROES: Hero[] = [
   {
     id: 12,
-    name: 'Dr. Nice'
+    name: 'Dr. Nice',
+    img: 'assets/img/drnice.png',
   },
   {
     id: 13,
-    name: 'Bombasto'
+    name: 'Bombasto',
+    img: 'assets/img/bombasto.png',
   },
   {
     id: 14,
-    name: 'Celeritas'
+    name: 'Celeritas',
+    img: 'assets/img/celeritas.png'
   },
   {
     id: 15,
-    name: 'Magneta'
+    name: 'Magneta',
+    img: 'assets/img/magneta.png'
   },
   {
     id: 16,
-    name: 'RubberMan'
+    name: 'RubberMan',
+    img: 'assets/img/rubberman.png'
   },
   {
     id: 17,
-    name: 'Dynama'
+    name: 'Dynama',
+    img: 'assets/img/dynama.png'
   },
   {
     id: 18,
-    name: 'Dr. IQ'
+    name: 'Dr. IQ',
+    img: 'assets/img/driq.png'
   },
   {
     id: 19,
-    name: 'Magma'
+    name: 'Magma',
+    img: 'assets/img/magma.png'
   },
   {
     id: 20,
-    name: 'Tornado'
+    name: 'Tornado',
+    img: 'assets/img/tornado.png'
   }
 ];
